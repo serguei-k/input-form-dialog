92674ad583bbf561010ad39c9ce3ac3b14a29103
Serguei Kalentchouk <serguei.k@hotmail.com>
2018-07-24 00:17:03 -0400

    fixed a file leak

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,16 @@
 import os
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     author='Serguei Kalentchouk',
     description='Input Form Dialog provides a simple multi-type input dialog for Qt5',
     install_requires=['Qt.py'],
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     name='input_form_dialog',
     package_dir={'input_form_dialog': 'src'},
     packages=['input_form_dialog'],
